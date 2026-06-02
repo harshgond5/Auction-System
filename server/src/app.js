40cb9f479b79cb3a8213d5d0648e99ca77ad7ca1
harsh <harshkrgond@gmail.com>
2026-06-02 13:58:00 +0530

    Intial project setup

---
+++ server/src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Auction Server Running");
+});
+
+connectDB();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+
+  socket.on("placeBid", (data) => {
+    io.emit("bidUpdated", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
